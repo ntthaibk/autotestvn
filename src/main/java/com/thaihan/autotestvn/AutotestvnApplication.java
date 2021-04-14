7d302eea074e4d6900907ed613001b8cb51d1a36
Thai Nguyen <62538035+ntthaibk@users.noreply.github.com>
2021-04-14 14:46:23 +0700

    init PJ

---
+++ src/main/java/com/thaihan/autotestvn/AutotestvnApplication.java
@@ -0,0 +1,13 @@
+package com.thaihan.autotestvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotestvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotestvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thaihan/autotestvn/AutotestvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaihan.autotestvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotestvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
